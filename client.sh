0448c4dde383c7bfaad0fd69d0963cfa30b38de1
David Gageot <david@gageot.net>
2014-11-19 15:26:09 -0800

    Client script

---
+++ client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! "${DOCKER_HOST}" =~ ^tcp://(.*):(.*)$ ]]; then
+	DOCKER_IP="localhost"
+else
+  DOCKER_IP=${BASH_REMATCH[1]}
+fi
+
+echo 'curl --data-binary @QR.rb http://${DOCKER_IP}:8080/run/ruby'
+curl --data-binary @QR.rb http://${DOCKER_IP}:8080/run/ruby
